bbc9e07cd88c7bc3b1e622fd2c1ac03feab89740
manaranjoc <man98@me.com>
2020-04-14 09:39:55 -0500

    Add resizing & blur methods for image preproccessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image, ImageFilter
 
 def loadImages(path, dataSet, dataType):
     image_files = sorted([
@@ -7,3 +8,16 @@ def loadImages(path, dataSet, dataType):
             if file.endswith('jpeg')
     ])
     return image_files
+
+def resizing(imageList, size):
+    importedImages = map(Image.open,imageList)
+    resizedImages = [
+        image.resize(size,Image.BILINEAR) for image in importedImages
+    ]
+    return resizedImages
+
+def gaussianBlur(resizedImages, radius):
+    blurImages = [
+        image.filter(ImageFilter.GaussianBlur(radius)) for image in resizedImages
+    ]
+    return blurImages
