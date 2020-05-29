2b6343d53e3ef5c5ed13ba53607c9eed7c115ead
Miguel Angel <angel.naranjo@udea.edu.co>
2020-05-29 14:35:20 -0500

    Add metric errors to code

--- kfolds/random_forest.py
+++ kfolds/random_forest.py
@@ -4,13 +4,21 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import sys
+from pathlib import Path
+sys.path[0] = str(Path(sys.path[0]).parent)
+
+from metrics import metrics, meanMetrics, printMetrics
+
 train_images = np.load('../saved_images/images_array_normal.npy')
 x = train_images[:,:-1]
 y = train_images[:,-1]
 
 print("Images already Loaded")
 
-kf = KFold(n_splits=10)
+num_splits = 10
+
+kf = KFold(n_splits=num_splits)
 kf.get_n_splits(x)
 
 error_by_B = np.zeros(5)
@@ -18,24 +26,33 @@ error_by_B = np.zeros(5)
 i = 0
 
 for B in range(10,51,10):
-    exactitud = 0
+
+    error_promedio = np.zeros((num_splits, 5))
+    iteration = 0
+
     clf = RandomForestClassifier(n_estimators=B)
+
     for train_index, test_index in kf.split(x):
         X_train, X_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
         clf.fit(X_train,y_train)
 
-        exactitud += clf.score(X_test, y_test)
+        y_pred = clf.predict(X_test)
 
-    error_promedio = 1-(exactitud/10)
+        error_promedio[iteration, :] = metrics(y_test, y_pred)
+        iteration += 1
 
-    print('Error para', B, ' forest: ',error_promedio)
+    error_promedio = meanMetrics(error_promedio)
 
-    error_by_B[i]=error_promedio
+    print('Error para', B, ' forest: ')
+    print('###########################################')
+    printMetrics(error_promedio)
+
+    error_by_B[i, :]=error_promedio
     i+=1
 
-plt.plot(range(10,51,10), error_by_B, 'b--')
+plt.plot(range(10,51,10), error_by_B[:, 0], 'b--')
 plt.xlabel('Numero de Arboles')
 plt.ylabel('Error asociado')
 plt.show()
--- kfolds/svm.py
+++ kfolds/svm.py
@@ -2,33 +2,51 @@ from sklearn.svm import LinearSVC
 from sklearn.model_selection import KFold
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+import sys
+from pathlib import Path
+sys.path[0] = str(Path(sys.path[0]).parent)
+
+from metrics import metrics, meanMetrics, printMetrics
+
 train_images = np.load('../saved_images/images_array_normal.npy')
 x = train_images[:,:-1]
 y = train_images[:,-1]
 
 C = np.logspace(1,2,2)
-Gamma = np.logspace(-3,2,6)
+#Gamma = np.logspace(-3,2,6)
 
-kf = KFold(n_splits=10)
+num_splits = 10
+
+kf = KFold(n_splits=num_splits)
 kf.get_n_splits(x)
 
-error_by_parameter = np.zeros((6,6))
+error_by_parameter = np.zeros((6,5))
 i = 0
 for c in C:
-        clf = LinearSVC(C=c, max_iter = 100000)
-        exactitud = 0
-        for train_index, test_index in kf.split(x):
-            X_train, X_test = x[train_index], x[test_index]
-            y_train, y_test = y[train_index], y[test_index]
+    clf = LinearSVC(C=c, max_iter = 100000)
 
-            clf.fit(X_train,y_train)
+    exactitud = np.zeros((num_splits, 5))
+    iteration = 0
 
-            exactitud += clf.score(X_test, y_test)
+    for train_index, test_index in kf.split(x):
+        X_train, X_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-        error_promedio = 1-(exactitud/10)
+        clf.fit(X_train,y_train)
 
-        print('Error para C=', c ,error_promedio)
+        y_pred = clf.predict(X_test)
 
-        error_by_parameter[int(i/5),i%5]=error_promedio
-        i += 1
+        exactitud[iteration, :] = metrics(y_test, y_pred)
+        iteration += 1
 
+    error_promedio = meanMetrics(error_promedio)
+
+    print('Error para C=', c)
+    printMetrics(error_promedio)
+
+    error_by_parameter[i,:]=error_promedio
+    i += 1
+
+plt.plot(C, error_by_parameter[:,0], 'b--')
